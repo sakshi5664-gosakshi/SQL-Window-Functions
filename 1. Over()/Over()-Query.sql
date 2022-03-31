29ee1d6875103004457567df8f8945f4d562a52c
Sakshi Singh <57246834+sakshi5664-gosakshi@users.noreply.github.com>
2022-03-31 13:18:35 +0530

    Create Over()-Query.sql

---
+++ 1. Over()/Over()-Query.sql
@@ -0,0 +1,16 @@
+-- Using Database
+
+Use [SQL-Window-Functions];
+
+-- Select the data from the database to extract the whole query set.
+
+Select * from dbo.[202202-capitalbikeshare-tripdata];
+
+-- The first part of the below aggregation, SUM(duration_seconds), looks a lot like basic aggregate function. 
+-- Adding OVER() designates it as a window function. 
+-- We can read the above aggregation as "take the sum of duration over the aggregate of entire result set."
+
+Select duration, SUM(duration) OVER() AS running_total from dbo.[202202-capitalbikeshare-tripdata];
+
+-- We can read the above aggregation as "take the sum of duration over the aggregate of entire result set, in order by started_at."
+Select duration, SUM(duration) OVER (ORDER BY started_at) AS running_total from dbo.[202202-capitalbikeshare-tripdata];
